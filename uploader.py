4f88806864b36421712e4a01a185371cafc31670
tanichi <tanichi@tanichisMacBook-Pro.local>
2018-11-12 01:22:37 +0900

    uploader.py

---
+++ uploader.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import io
+import tornado.web
+from datetime import datetime 
+
+class PostHandler(tornado.web.RequestHandler):
+    def post(self):
+        print(datetime.now().strftime("%H:%M:%S"), self.request.files['file'][0]['filename'])
+        data = io.BytesIO(self.request.files['file'][0]['body'])
+        f = open("upload/" + self.request.files['file'][0]['filename'], 'w')
+        
+        for line in data:
+            f.write(line.decode('utf-8'))
+
+# WebAPIの起動
+application = tornado.web.Application([
+    (r"/", PostHandler)
+])
+
+if __name__ == "__main__":
+    application.listen(8000)
+    tornado.ioloop.IOLoop.instance().start()
